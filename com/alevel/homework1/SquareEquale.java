d1a19b7654a7192732a547b9dba300ea07e4cc0c
kyslukha <kyslukha@gmail.com>
2020-12-17 18:59:51 +0200

    second commit

--- com/alevel/homework1/SquareEquale.java
+++ com/alevel/homework1/SquareEquale.java
@@ -3,19 +3,19 @@ package com.alevel.homework1;
 public class SquareEquale {
     public static void main(String[] args) {
         int a = 6;
-        int b = 12;
+        int b = 17;
         int c = 6;
 
-        double Discriminant = b*b - (4*c*a);
-        if (Discriminant<0) System.out.println("No solutions");
+        int discriminant = b*b - (4*c*a);
+        if (discriminant<0) System.out.println("No solutions");
         else
         {
-            if(Discriminant==0) System.out.println("Solution is " + (-b/(2*a)));
+            if(discriminant==0) System.out.println("Solution is " + (-b/(2*a)));
             else
             {
                 System.out.println("Solution are");
-                System.out.println((-b+ Math.sqrt(Discriminant))/(2*a));
-                System.out.println((-b- Math.sqrt(Discriminant))/(2*a));
+                System.out.println((-b+ Math.sqrt(discriminant))/(2*a));
+                System.out.println((-b- Math.sqrt(discriminant))/(2*a));
             }
         }
 
