97a41de86fa1770ed594e22c3769b8cdc1eace9b
kyslukha <kyslukha@gmail.com>
2020-12-18 21:02:19 +0200

    remove comment, comment for code, add case for no area triangle, change type of variable

--- com/alevel/homework1/Code.java
+++ com/alevel/homework1/Code.java
@@ -1,14 +1,53 @@
 package com.alevel.homework1;
+//The code word defines the letter shift of the coded word in alphabetical order
+//        For example, if the code word is DIMA. We encode the word MOBILE
+//M -- P
+//O -- W
+//B -- N
+//I -- I If the code word ends, start with the first letter of the code word
+//L -- O
+//E –- M
+// Coded word is Mobile -- New word is Pwniom (look tab)
+//letters of| letters of
+//coded word| new word
+//        a	|d	i	m	a
+//        b	|e	j	n	b
+//        c	|f	k	o	c
+//        d	|g	l	p	d
+//        e	|h	m	q	e
+//        f	|i	n	r	f
+//        g	|j	o	s	g
+//        h	|k	p	t	h
+//        i	|l	q	u	i
+//        j	|m	r	v	j
+//        k	|n	s	w	k
+//        l	|o	t	x	l
+//        m	|p	u	y	m
+//        n	|q	v	z	n
+//        o	|r	w	a	o
+//        p	|s	x	b	p
+//        q	|t	y	c	q
+//        r	|u	z	d	r
+//        s	|v	a	e	s
+//        t	|w	b	f	t
+//        u	|x	c	g	u
+//        v	|y	d	h	v
+//        w	|z	e	i	w
+//        x	|a	f	j	x
+//        y	|b	g	k	y
+//        z	|c	h	l	z
+//
 
 public class Code {
     public static void main(String[] args) {
         char[] code = {'d', 'i', 'm', 'a'};
         char[] word = {'h', 'e', 'l', 'l', 'o'};
-        System.out.print("code =  ");
+        System.out.print("Code word is ");
         for (int i = 0; i < 4; i++) {
             System.out.print(code[i]);
         }
-        System.out.print("  word = ");
+        System.out.println();
+        System.out.print("Coded word is ");
         for (int i = 0; i < 5; i++) {
             System.out.print(word[i]);
         }
@@ -19,25 +58,22 @@ public class Code {
         }
 
         for (int i = 0; i < 5; i++) {
-            int w=0;
+            int w = 0;
             for (int k = 0; word[i] != alphabet[k]; k++) {
-               // System.out.println("k = " + k);
-                w=w+1;
+                // System.out.println("k = " + k);
+                w = w + 1;
             }
-            int c=0;
-            for (int j = 0; code[i%4] != alphabet[j]; j++) {
+            int c = 0;
+            for (int j = 0; code[i % 4] != alphabet[j]; j++) {
                 //System.out.println("j = " + j);
-                c=c+1;
+                c = c + 1;
             }
-            codedWord[i] = alphabet[(c+w)%26];
+            codedWord[i] = alphabet[(c + w) % 26];
         }
-        System.out.print("  coded word =  ");
-     for (int i=0; i<5; i++){
+        System.out.println();
+        System.out.print("New word is ");
+        for (int i = 0; i < 5; i++) {
             System.out.print(codedWord[i]);
-      }
-
-        //System.out.println(Arrays.toString(code));
-
-
-    }
+        }
+   }
 }
--- com/alevel/homework1/SquareEquale.java
+++ com/alevel/homework1/SquareEquale.java
@@ -3,22 +3,17 @@ package com.alevel.homework1;
 public class SquareEquale {
     public static void main(String[] args) {
         int a = 6;
-        int b = 17;
+        int b = 2;
         int c = 6;
-
-        int discriminant = b*b - (4*c*a);
-        if (discriminant<0) System.out.println("No solutions");
-        else
-        {
-            if(discriminant==0) System.out.println("Solution is " + (-b/(2*a)));
-            else
-            {
-                System.out.println("Solution are");
-                System.out.println((-b+ Math.sqrt(discriminant))/(2*a));
-                System.out.println((-b- Math.sqrt(discriminant))/(2*a));
+        int discriminant = b * b - (4 * c * a);
+        if (discriminant < 0) System.out.println("No solutions");
+        else {
+            if (discriminant == 0) System.out.println("Solution is " + (-b / (2 * a)));
+            else {
+                System.out.println("Solution are ");
+                System.out.println((-b + Math.sqrt(discriminant)) / (2 * a));
+                System.out.println((-b - Math.sqrt(discriminant)) / (2 * a));
             }
         }
-
-
     }
 }
